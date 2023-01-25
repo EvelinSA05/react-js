d74180796c4826edd9a7f5961841b5134590648e
Evelin Salsabila A <108512588+EvelinSA05@users.noreply.github.com>
2023-01-25 17:56:12 +0700

    project react resto bagian 18

---
+++ react-resto-bag18/layout/link.js
@@ -0,0 +1,9 @@
+const url = "http://localhost:8000/api";
+let token = "7AvoXxYp2FfgAzvvUnjks5xe7UNTqCWHr3txdjbj";
+
+export const link = axios.create({
+    baseURL: url,
+    headers: {
+        'api_token': token
+    }
+});
